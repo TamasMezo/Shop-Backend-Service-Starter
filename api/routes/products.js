8232f128f8e2f0f97f3ebfb214940b632aee9016
Tamás <codesigner.hu@gmail.com>
2019-12-15 19:29:37 +0100

    improving product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   res.status(200).json({
@@ -8,28 +11,38 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
-    name: req.body.name || "",
-    price: req.body.price || ""
-  };
-  res.status(201).json({
-    message: "Handeling post requests to products",
-    createdProduct: product
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handeling post requests to products",
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "Spec. product get req. works",
-      id: id
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      res.status(200).json(doc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "you passed an id"
-    });
-  }
 });
 
 router.patch("/:productId", (req, res, next) => {
