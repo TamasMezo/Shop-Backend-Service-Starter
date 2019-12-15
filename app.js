eae92ee4646125f3fb1f9cc7566b7b80014cd412
Tamás <codesigner.hu@gmail.com>
2019-12-15 13:37:48 +0100

    adding orders routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
 /* app.use((req, res, next) => {
   res.status(200).json({
@@ -10,5 +11,6 @@ const productRoutes = require("./api/routes/products");
 }); */
 
 app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
