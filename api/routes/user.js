bfdcac34b33f8ed485f55e6b214e2644894a4b19
Tamás <codesigner.hu@gmail.com>
2020-01-03 22:36:58 +0100

    lil fixes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
-const User = require("../models/user");
+const checkAuth = require("../middleware/check-auth");
 
 const UserController = require("../controllers/user");
 
@@ -12,6 +8,6 @@ router.post("/signup", UserController.user_signup);
 
 router.post("/login", UserController.user_login);
 
-router.delete("/:userId", UserController.user_delete);
+router.delete("/:userId", checkAuth, UserController.user_delete);
 
 module.exports = router;
